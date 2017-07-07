bd1d7cc8d07342d9e4dd1bc59173cd98f89c61dc
NStephenson <nickstephenson90@gmail.com>
2017-07-06 22:58:49 -0400

    started basic poll form component

---
+++ src/app/poll-form/poll-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poll-form',
+  templateUrl: './poll-form.component.html',
+  styleUrls: ['./poll-form.component.css']
+})
+export class PollFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
