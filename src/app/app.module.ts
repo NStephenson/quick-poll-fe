8697cc465a3efd357d104a7d35ae3d9bc87f0b2a
NStephenson <nickstephenson90@gmail.com>
2017-07-06 22:57:05 -0400

    added basic modules and compnents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 import { AppComponent } from './app.component';
+import { PollFormComponent } from './poll-form/poll-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PollFormComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
