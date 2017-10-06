6c10fd0476d55ec948b21504c01e1793f069b961
NStephenson <nickstephenson90@gmail.com>
2017-10-06 18:38:16 -0400

    added functions to allow poll creation

--- src/app/poll/poll-new/poll-new.component.ts
+++ src/app/poll/poll-new/poll-new.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Poll } from '../poll';
+import { PollService } from '../poll.service';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-poll-new',
   templateUrl: './poll-new.component.html',
-  styleUrls: ['./poll-new.component.css']
+  styleUrls: ['./poll-new.component.css'],
+  providers: [PollService]
 })
 export class PollNewComponent implements OnInit {
+  poll: Poll = new Poll();
+  submitted: boolean = false; 
 
-  constructor() { }
+
+  constructor(private pollService: PollService){
+  }
 
   ngOnInit() {
+    this.poll.responses = [];
+    this.addResponse()
   }
+
+  addResponse(){
+    if (!this.responseLimit()){
+      this.poll.responses.push({text: ''})
+    }
+  }
+
+  createPoll(poll){
+    this.submitted = true;
+    this.pollService.createPoll(poll)
+        .subscribe(data => {return true;}, 
+                   error => { console.log("error saving");
+                              return Observable.throw(error);
+                            });
+  }
+
+  responseLimit(){
+    return this.poll.responses.length >= 6
+  }
+
 
 }
