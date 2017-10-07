c3285efc83a7c9e31ad5d54dcb197560f98dd0fd
NStephenson <nickstephenson90@gmail.com>
2017-10-06 21:34:57 -0400

    added import for poll show component

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -5,10 +5,11 @@ import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from '../home/home.component'
 
 
-
+import { PollShowComponent } from '../poll/poll-show/poll-show.component'
 import { PollFormComponent } from '../poll/poll-form/poll-form.component'
 import { PollResultsComponent } from '../poll/poll-results/poll-results.component'
 import { PollComponent } from '../poll/poll.component'
+
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
