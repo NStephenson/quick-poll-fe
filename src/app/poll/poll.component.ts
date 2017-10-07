5f2af1e2dd85b4cbc62dc6fd1fb0579e2cd13a75
NStephenson <nickstephenson90@gmail.com>
2017-10-06 22:32:30 -0400

    poll component now recieves poll as input

--- src/app/poll/poll.component.ts
+++ src/app/poll/poll.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Component, OnInit, Input } from '@angular/core';
 import { Poll }  from './poll'
-import { Observable } from 'rxjs/Rx'
-import { PollService } from './poll.service'
 
 @Component({
   selector: 'app-poll',
@@ -10,28 +7,16 @@ import { PollService } from './poll.service'
   styleUrls: ['./poll.component.css']
 })
 
-export class PollComponent implements OnInit {
-  poll: Poll;
-  errorMessage: string;
-  mode = "Observable";
 
+export class PollComponent implements OnInit {
+
+  @Input() 
+  poll: Poll;
 
   constructor(
-    private pollService: PollService,
-    private router: Router
   ){}
 
   ngOnInit(){
-    let timer = Observable.timer(0)
-    timer.subscribe(() => this.getPoll())
-  }
-
-  getPoll(){
-    this.pollService.getPoll(2)
-        .subscribe(
-          poll => this.poll = poll,
-          error => this.errorMessage = <any>error
-        )
   }
 
 }
