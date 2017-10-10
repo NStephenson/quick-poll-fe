b53c3d37d8b6a6d5c112fd73bfc7c3bdc35b70bf
NStephenson <nickstephenson90@gmail.com>
2017-10-10 16:46:52 -0400

    added functions and event triggers to make poll form submittable

--- src/app/poll/poll-form/poll-form.component.ts
+++ src/app/poll/poll-form/poll-form.component.ts
@@ -1,17 +1,53 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Poll } from '../poll'
+import { PollService } from '../poll.service'
+import { Observable } from 'rxjs/Rx'
+
+
 
 @Component({
   selector: 'app-poll-form',
   templateUrl: './poll-form.component.html',
-  styleUrls: ['./poll-form.component.css']
+  styleUrls: ['./poll-form.component.css'],
+  providers: [PollService]
 })
 export class PollFormComponent implements OnInit {
-  @Input() poll: Poll;
+  selection: number[] = [];
 
-  constructor() { }
+  @Input()
+  poll: Poll;
+
+  constructor(private pollService: PollService) { }
 
   ngOnInit() {
+  }
+
+  handleSubmit(){
+    this.pollService.submitResponses(this.poll.id, this.selection)
+                    .subscribe(data => {return true;}, 
+                               error => { console.log("error saving");
+                               return Observable.throw(error);
+                              });
+  }
+
+  handleSelection(id){
+    if (this.poll.select_multiple){
+      this.toggleCheckbox(id);
+    } else {
+      this.toggleRadio(id);
+    }
+  }
+
+  toggleCheckbox(id){
+    if (this.selection.includes(id)){
+      this.selection = this.selection.filter(x => x != id);
+    } else {
+      this.selection.push(id);
+    }
+  }
+
+  toggleRadio(id){
+    this.selection = [id];
   }
 
 }
