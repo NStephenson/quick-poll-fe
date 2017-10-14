79033e2630ee0b7151ee4e22bbd124cd1d604993
NStephenson <nickstephenson90@gmail.com>
2017-10-14 01:58:19 -0400

    when poll results are shown, poll will retrieve it's results via observable

--- src/app/poll/poll.component.ts
+++ src/app/poll/poll.component.ts
@@ -1,28 +1,50 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Poll }  from './poll'
+import { Poll }  from './poll';
+import { Observable } from 'rxjs/Rx';
+import { PollService }  from './poll.service';
+import { ISubscription } from "rxjs/Subscription";
+
 
 @Component({
   selector: 'app-poll',
   templateUrl: './poll.component.html',
-  styleUrls: ['./poll.component.css']
+  styleUrls: ['./poll.component.css'],
+  providers: [PollService]
 })
 
 
 export class PollComponent implements OnInit {
   results: boolean = false
+  errorMessage: string;
+  private subscription: ISubscription;
 
-  @Input() 
-  poll: Poll;
 
-  constructor(
+  @Input() poll: Poll;
+
+  constructor(private pollService: PollService
   ){}
 
   ngOnInit(){
   }
 
   toggleResults(e){
-    e.preventDefault()
-    this.results = !this.results
+    e ? e.preventDefault() : null
+    this.results = !this.results;
+    
+    if (this.results){
+      let timer = Observable.timer(0, 5000)
+      this.subscription = timer.subscribe(() => this.getPoll());
+    } else {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  getPoll(){
+    this.pollService.getPoll(this.poll.id)
+                    .subscribe(
+                      poll => this.poll = poll,
+                      error => this.errorMessage = <any>error
+                    )
   }
 
 }
