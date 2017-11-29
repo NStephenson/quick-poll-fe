0eea273d243e59c389da48cb77bfe0f6920821ec
NStephenson <nickstephenson90@gmail.com>
2017-11-28 20:24:11 -0500

    added auth components, poll index, and angular token service

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,23 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { Angular2TokenService } from 'angular2-token';
 
 import { HomeComponent } from '../home/home.component';
 import { RegistrationComponent } from '../session/registration/registration.component'
-
+import { SignInComponent } from '../session/sign-in/sign-in.component'
+import { NotFoundComponent } from '../not-found/not-found.component'
 
 import { PollShowComponent } from '../poll/poll-show/poll-show.component'
 import { PollFormComponent } from '../poll/poll-form/poll-form.component'
 import { PollResultsComponent } from '../poll/poll-results/poll-results.component'
 import { PollComponent } from '../poll/poll.component'
+import { PollIndexComponent } from '../poll/poll-index/poll-index.component';
+
+
+import { Angular2TokenService } from 'angular2-token';
+
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'results', component: PollResultsComponent},
-  {path: 'polls/:id', component: PollShowComponent},
-  {path: 'register', component: RegistrationComponent}
+  {path: 'polls/:id', component: PollShowComponent, canActivate: [Angular2TokenService]},
+  {path: 'polls', component: PollIndexComponent},
+  {path: 'register', component: RegistrationComponent},
+  {path: 'sign-in', component: SignInComponent},
+  {path: '**', component: NotFoundComponent}
 ]
 
 
@@ -31,4 +39,13 @@ const routes: Routes = [
   declarations: []
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {
+    constructor(private _tokenService: Angular2TokenService) {
+        this._tokenService.init({
+          apiBase: "http://localhost:3000",
+          signInPath: '',
+          signInRedirect: ''
+        });
+    }
+
+ }
