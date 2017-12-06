fd642342208a643475aedc770ab8bf19928607eb
NStephenson <nickstephenson90@gmail.com>
2017-12-06 11:42:46 -0500

    added guards for routes and removed unecessary constructor

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -14,7 +14,7 @@ import { PollComponent } from '../poll/poll.component'
 import { PollIndexComponent } from '../poll/poll-index/poll-index.component';
 
 
-import { Angular2TokenService } from 'angular2-token';
+import { AuthGuard, UnAuthGuard } from '../guards/auth.guard'
 
 
 
@@ -22,9 +22,9 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'results', component: PollResultsComponent},
   {path: 'polls/:id', component: PollShowComponent},
-  {path: 'polls', component: PollIndexComponent, canActivate: [Angular2TokenService]},
-  {path: 'register', component: RegistrationComponent},
-  {path: 'sign-in', component: SignInComponent},
+  {path: 'polls', component: PollIndexComponent, canActivate: [AuthGuard]},
+  {path: 'register', component: RegistrationComponent, canActivate: [UnAuthGuard]},
+  {path: 'sign-in', component: SignInComponent, canActivate: [UnAuthGuard]},
   {path: '**', component: NotFoundComponent}
 ]
 
@@ -40,12 +40,4 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule {
-    constructor(private _tokenService: Angular2TokenService) {
-        this._tokenService.init({
-          apiBase: "http://localhost:3000",
-          signInPath: '',
-          signInRedirect: ''
-        });
-    }
-
  }
