895d6bd6de0e2477255239402a93eeb27768feb5
NStephenson <nickstephenson90@gmail.com>
2018-03-22 12:29:14 -0400

    handled poll editing and deletion

--- src/app/poll/poll-results/poll-results.component.ts
+++ src/app/poll/poll-results/poll-results.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Poll } from '../poll'
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-poll-results',
@@ -8,13 +9,24 @@ import { Poll } from '../poll'
 })
 export class PollResultsComponent implements OnInit {
 
-  @Input()
-  poll: Poll;
+  @Input() poll: Poll;
+  @Input() editable: boolean;
   @Output() toggleResults: EventEmitter<any> = new EventEmitter()
+  @Output() handleEdit: EventEmitter<any> = new EventEmitter()
+  @Output() handleDelete: EventEmitter<any> = new EventEmitter()
 
-  constructor() { }
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  emitEdit(poll){
+    this.handleEdit.emit(poll)
+  }
+
+  emitDelete(pollId){
+    this.handleDelete.emit(pollId)
   }
 
    seeResults(){
