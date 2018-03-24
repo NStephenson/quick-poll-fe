f3303e36a50662fe0edd8e6000e864565500c3fe
NStephenson <nickstephenson90@gmail.com>
2018-03-24 12:11:11 -0400

    made registration flow match sign in flow

--- src/app/session/registration/registration.component.ts
+++ src/app/session/registration/registration.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Angular2TokenService } from 'angular2-token';
 import { NewUser } from './newUser'
+import { Router } from '@angular/router'
 
 
 @Component({
@@ -12,15 +13,21 @@ export class RegistrationComponent implements OnInit {
   newUser: NewUser = {};
   errors: any;
 
-  constructor(private tokenService: Angular2TokenService) {}
+  constructor(
+    private tokenService: Angular2TokenService,
+    private router: Router
+    ) {}
 
   ngOnInit() {
   }
 
   signUp(newUser){
     this.tokenService.registerAccount(newUser).subscribe(
-      res =>      console.log(res),
-      error =>   this.errors= JSON.parse(error._body).errors
+      res =>    {
+        this.tokenService.validateToken(); 
+        this.router.navigate(['polls'])
+      },
+      error =>  this.errors = JSON.parse(error._body).errors
     );
   }
 
